bde5e389e81845f09753f175b32403216dd52faa
Ben Cutrell <bcutrell@Bens-Macbook.local>
2016-11-22 17:53:58 -0500

    more style changes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
+const ourStoryHtml = " \
+<div class='row'> \
+<div class='col-md-12'> \
+<p>They met at the gym.</p> \
+</div> \
+</div>"
+
+
 $( document ).ready(function() {
-    
     loadScript("js/sam.js", cb);
 
-
-    // test
     function cb() {
         sam()
     }
-  
+    $("#navbarList li").click(function(evt) {
+      var ele = evt.target;
+      $(ele).addClass('clicked');
+      $(this).siblings().children().removeClass('clicked')
+
+      $('#bodyContent').html(ourStoryHtml)
+
+    })
 });
-
-
-
 
 function loadScript(url, callback)
 {
